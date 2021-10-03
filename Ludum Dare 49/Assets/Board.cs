cba855dc28126ef388cb8c6f66a5ddbbb145380d
zinnin <34364024+zinnin@users.noreply.github.com>
2021-10-02 20:03:06 -0700

    nah though

--- Ludum Dare 49/Assets/Board.cs
+++ Ludum Dare 49/Assets/Board.cs
@@ -153,7 +153,6 @@ public class Board : MonoBehaviour
 
         GameActive = false;
         SoundDesign.BeginLoseTheme();
-        SoundDesign.PlayOptionSelected();
     }
 
     public void ResetBoard()
